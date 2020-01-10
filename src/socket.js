54b60016c4f1809aaddf979d5fca22a74407af23
Daniel <danielrs9705@gmail.com>
2020-01-10 18:03:12 +0100

    Formatting documents

--- src/socket.js
+++ src/socket.js
@@ -1,45 +1,52 @@
-const { docs,  addUserToDoc, removeUserOfDoc, getUserAndSaveCoords, getUsersOfDoc, getUser } = require('./utils/document');
+const {
+	docs,
+	addUserToDoc,
+	removeUserOfDoc,
+	getUserAndSaveCoords,
+	getUsersOfDoc,
+	getUser
+} = require('./utils/document');
 
 module.exports = (io) => {
-    io.on('connection', (socket) => {
-        // console.log("A new user arrived", socket.id);
-    
-        socket.on('join', (options, callback) => {
-            // First we add the user to the document
-            const { error, user } = addUserToDoc({socket_id: socket.id, ...options});
-            if (error) {
-                return;
-            }
-            socket.join(user.docId);
-            socket.to(user.docId).broadcast.emit('notification', `User ${user.socket_id} has joined!`);
-            
-            const doc = docs.find((doc) => doc._id === user.docId);
-            socket.emit('change', doc);
-            callback(socket.id);
-            socket.emit('user-new-position', getUsersOfDoc(null, user.docId));
-        })
-    
-        // Documents events
-        socket.on('save', (newDoc) => {
-            const doc = docs.find((doc) => doc._id === newDoc._id);
-            // console.log("Updating the doc", newDoc);
-            doc.content = newDoc.content;
-            io.to(doc._id).emit('change', doc);
-        })
-        
-        socket.on('update-cursor-position', ({docId, coords}) => {
-            const user = getUserAndSaveCoords(socket.id, docId, coords); 
-            if (user) {
-                socket.to(user.docId).broadcast.emit('user-new-position', getUsersOfDoc(null, docId));
-            }
-        })
+	io.on('connection', (socket) => {
+		// console.log("A new user arrived", socket.id);
 
-        socket.on('disconnect', () => {
-            const user = removeUserOfDoc(socket.id);
-            if (user) {
-                io.to(user.docId).emit('notification', `User ${user.socket_id} has left!!`); 
-                io.to(user.docId).emit('user-leave', {users: getUsersOfDoc(null, user.docId), userRemoved: user});
-            }
-        });
-    })
-}
+		socket.on('join', (options, callback) => {
+			// First we add the user to the document
+			const { error, user } = addUserToDoc({ socket_id: socket.id, ...options });
+			if (error) {
+				return;
+			}
+			socket.join(user.docId);
+			socket.to(user.docId).broadcast.emit('notification', `User ${user.socket_id} has joined!`);
+
+			const doc = docs.find((doc) => doc._id === user.docId);
+			socket.emit('change', doc);
+			callback(socket.id);
+			socket.emit('user-new-position', getUsersOfDoc(null, user.docId));
+		});
+
+		// Documents events
+		socket.on('save', (newDoc) => {
+			const doc = docs.find((doc) => doc._id === newDoc._id);
+			// console.log("Updating the doc", newDoc);
+			doc.content = newDoc.content;
+			io.to(doc._id).emit('change', doc);
+		});
+
+		socket.on('update-cursor-position', ({ docId, coords }) => {
+			const user = getUserAndSaveCoords(socket.id, docId, coords);
+			if (user) {
+				socket.to(user.docId).broadcast.emit('user-new-position', getUsersOfDoc(null, docId));
+			}
+		});
+
+		socket.on('disconnect', () => {
+			const user = removeUserOfDoc(socket.id);
+			if (user) {
+				io.to(user.docId).emit('notification', `User ${user.socket_id} has left!!`);
+				io.to(user.docId).emit('user-leave', { users: getUsersOfDoc(null, user.docId), userRemoved: user });
+			}
+		});
+	});
+};
--- src/utils/document.js
+++ src/utils/document.js
@@ -1,59 +1,59 @@
 // This docs constant is going to represent
 // the docs that are currently loaded and being modify
 const docs = [
-    {
-        _id: '123',
-        modifyAt: new Date(),
-        content: '',
-    },
-    {
-        _id: '1234',
-        modifyAt: new Date(),
-        content: 'safansofisanonsaksaon',
-    }
-]
+	{
+		_id: '123',
+		modifyAt: new Date(),
+		content: ''
+	},
+	{
+		_id: '1234',
+		modifyAt: new Date(),
+		content: 'safansofisanonsaksaon'
+	}
+];
 
 // This users constants is going to content all the users
 // connected to a document
-const users = []
+const users = [];
 
 /**
  * This function adds a user to a document
  * @param {*} docId The id of the document to edit
  * @param {*} newUser The user who is going to join
  */
-const addUserToDoc = (newUser) => { 
-    const user = users.find((user) => user.socket_id === newUser.socket_id && user.docId === newUser.docId);
-    if (user) {
-        return {
-            error: 'The user is already in'
-        }
-    }
+const addUserToDoc = (newUser) => {
+	const user = users.find((user) => user.socket_id === newUser.socket_id && user.docId === newUser.docId);
+	if (user) {
+		return {
+			error: 'The user is already in'
+		};
+	}
 
-    users.push(newUser);
-    return {
-        user: newUser
-    };
-}
+	users.push(newUser);
+	return {
+		user: newUser
+	};
+};
 
 /**
  * This function remove a user from a document
  * @param {*} userId 
  */
 const removeUserOfDoc = (userId) => {
-    const index = users.findIndex((user) => user.socket_id === userId);
-    if (index !== -1) {
-        return users.splice(index, 1)[0];
-    }
-}
+	const index = users.findIndex((user) => user.socket_id === userId);
+	if (index !== -1) {
+		return users.splice(index, 1)[0];
+	}
+};
 
 /**
  * This function get a user given an socket id
  * @param {*} userId The id of the user to obtain
  */
 const getUser = (userId) => {
-    return users.find((user) => user.socket_id === userId);
-}
+	return users.find((user) => user.socket_id === userId);
+};
 
 /**
  * This method set the new position of user's cursor
@@ -61,27 +61,27 @@ const getUser = (userId) => {
  * @param {*} coords the new coords to set
  */
 const getUserAndSaveCoords = (userId, docId, coords) => {
-    const index = users.findIndex((user) => user.socket_id === userId && user.docId === docId);
-    if (index !== -1) {
-        users[index].coords = coords;
-        return users[index];
-    }
-}
+	const index = users.findIndex((user) => user.socket_id === userId && user.docId === docId);
+	if (index !== -1) {
+		users[index].coords = coords;
+		return users[index];
+	}
+};
 
 /**
  * This method gets all the user in a given doc
  * @param {*} docId The id of the doc
  */
-const getUsersOfDoc = (userId=null, docId) => {
-    if (userId === null) return users.filter((user) => user.docId === docId);
-    return users.filter((user) => user.docId === docId && user.socket_id !== userId);
-}
+const getUsersOfDoc = (userId = null, docId) => {
+	if (userId === null) return users.filter((user) => user.docId === docId);
+	return users.filter((user) => user.docId === docId && user.socket_id !== userId);
+};
 
 module.exports = {
-    docs,
-    addUserToDoc,
-    removeUserOfDoc,
-    getUser,
-    getUserAndSaveCoords,
-    getUsersOfDoc
-}
+	docs,
+	addUserToDoc,
+	removeUserOfDoc,
+	getUser,
+	getUserAndSaveCoords,
+	getUsersOfDoc
+};
