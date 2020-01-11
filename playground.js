75aa2e8e3600104ed46213ac8e59ed9584a5ed08
Daniel <danielrs9705@gmail.com>
2020-01-11 13:56:40 +0100

    Some important info about OT

--- playground.js
+++ playground.js
@@ -1,9 +1,28 @@
 const ot = require('ot');
 
-let document = 'iao';
-console.log(document);
-const operation = new ot.TextOperation().retain(1).insert('a').retain(document.length - 1);
+let document = 'iao\n\nHola';
+// console.log(document);
+// const operation = new ot.TextOperation().retain(1).insert('a').retain(document.length - 1);
 
-document = operation.apply(document);
+// document = operation.apply(document);
+// console.log(document);
+// const operation_2 = new ot.TextOperation().retain(1).delete('a').retain(document.length - 2);
 
-console.log(document);
+// document = operation_2.apply(document);
+
+// console.log(document);
+
+const executeOperation = (operation) => {
+	const newDocument = operation.apply(document);
+	return newDocument;
+};
+
+const str = 'hola';
+const position = 4;
+document = executeOperation(new ot.TextOperation().retain(position).insert(str).retain(document.length - position));
+console.log("Inserting 'hola': ", document);
+
+document = executeOperation(
+	new ot.TextOperation().retain(position).delete(str).retain(document.length - position - str.length)
+);
+console.log("Deleting 'hola': ", document);
